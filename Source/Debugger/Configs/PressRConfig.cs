9221ad5797eca1134e473f844c3edb985620ba56
alxraun <76945099+alxraun@users.noreply.github.com>
2025-05-05 00:14:08 +0300

    Add PressR configuration class for debugger tracking
    - Introduced `PressRConfig` class implementing `IDebuggerConfig` for managing debugger settings.
    - Defined properties for configuration such as `Name`, `UpdateInterval`, and `StartDelaySeconds`.
    - Implemented `GetTrackedValues` method to return a list of tracked values related to PressR input and graphics management.
    - Enhanced the debugging capabilities by providing structured tracking of key game states and graphics objects.

--- Source/Debugger/Configs/PressRConfig.cs
+++ Source/Debugger/Configs/PressRConfig.cs
@@ -6,29 +6,30 @@ using static PressR.Debugger.DebuggerTrackedValueInfo;
 
 namespace PressR.Debugger.Configs
 {
-    public class GraphicsManagerConfig : IDebuggerConfig
+    public class PressRConfig : IDebuggerConfig
     {
-        public string Name => "Graphics Manager State";
+        public string Name => "PressR State";
 
-        public float UpdateInterval => 0.1f;
+        public float UpdateInterval => 0.05f;
 
-        public float StartDelaySeconds => 0f;
+        public float StartDelaySeconds => 3f;
 
         public IEnumerable<DebuggerTrackedValueInfo> GetTrackedValues()
         {
             return new List<DebuggerTrackedValueInfo>
             {
+                TrackValue(
+                    "PressR.PressRInput.IsPressRModifierKeyPressed",
+                    "IsPressRModifierKeyPressed"
+                ),
+                TrackValue("PressR.PressRInput.IsMouseButtonHeld", "IsMouseButtonHeld"),
                 TrackCollectionCount(
                     "PressR.PressRMain._graphicsManager._graphicObjects",
                     "Graphic Objects"
                 ),
                 TrackCollectionCount(
-                    "PressR.PressRMain._graphicsManager._activeEffects",
-                    "Active Effects"
-                ),
-                TrackCollectionCount(
-                    "PressR.PressRMain._graphicsManager._objectToActiveEffectIds",
-                    "GO to AE IDs"
+                    "PressR.PressRMain._graphicsManager._activeTweens",
+                    "Active Tweens"
                 ),
             };
         }
